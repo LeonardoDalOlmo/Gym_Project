35abcf028fb6384188324ce6e65b977f5b6ef9d5
Leonardo Dal Olmo <leo.dalolmoofc@gmail.com>
2024-10-04 20:37:12 -0300

    Creation of project

---
+++ Gym Project/Gym-Project/src/main/java/com/example/Gym/Project/GymProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gym.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymProjectApplication.class, args);
+	}
+
+}
---
+++ Gym Project/Gym-Project/src/test/java/com/example/Gym/Project/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gym.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
