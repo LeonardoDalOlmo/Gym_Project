7abf66225b8e42a3f0b04de0fd0c0516f575ed84
Pedro Rafael de Souza e Silva <pedrorafaelofficial@gmail.com>
2024-11-15 11:05:53 -0300

    Criei o restante das classes Controller
    Alterei o método findAll() das classes recomendadas pelo IntelliJ

---
+++ Gym Project/Gym-Project/src/main/java/com/example/Gym/Project/Controller/PeriodController.java
@@ -0,0 +1,57 @@
+package com.example.Gym.Project.Controller;
+
+import com.example.Gym.Project.DTO.PeriodDTO;
+import com.example.Gym.Project.Service.PeriodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/periods")
+public class PeriodController {
+
+    @Autowired
+    private PeriodService service;
+
+    @GetMapping
+    public ResponseEntity<List<PeriodDTO>> findAll(){
+        var periods = service.findAll();
+
+        return ResponseEntity.ok().body(periods);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PeriodDTO> findById(@PathVariable Integer id) {
+        var periods = service.findById(id);
+
+        return ResponseEntity.ok().body(periods);
+    }
+
+    @PostMapping
+    public ResponseEntity<PeriodDTO> insert(@RequestBody PeriodDTO dto){
+        var period = service.insertPeriod(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/id")
+                .buildAndExpand(period.getPeriodId()).toUri();
+
+        return ResponseEntity.created(uri).body(period);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PeriodDTO> update(@PathVariable Integer id, @RequestBody PeriodDTO dto){
+        var period = service.updatePeriod(id, dto);
+
+        return ResponseEntity.ok().body(period);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.deletePeriod(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ Gym Project/Gym-Project/src/main/java/com/example/Gym/Project/Controller/PlanController.java
@@ -0,0 +1,58 @@
+package com.example.Gym.Project.Controller;
+
+import com.example.Gym.Project.DTO.PlanDTO;
+import com.example.Gym.Project.Service.PlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/plans")
+public class PlanController {
+
+    @Autowired
+    private PlanService service;
+
+    @GetMapping
+    public ResponseEntity<List<PlanDTO>> findAll(){
+        var plans = service.findAll();
+
+        return ResponseEntity.ok().body(plans);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlanDTO> findById(@PathVariable Integer id) {
+        var plans = service.findById(id);
+
+        return ResponseEntity.ok().body(plans);
+    }
+
+    @PostMapping
+    public ResponseEntity<PlanDTO> insert(@RequestBody PlanDTO dto){
+        var plan = service.insertPlan(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/id")
+                .buildAndExpand(plan.getPlanId()).toUri();
+
+        return ResponseEntity.created(uri).body(plan);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlanDTO> update(@PathVariable Integer id, @RequestBody PlanDTO dto){
+        var plan = service.updatePlan(id, dto);
+
+        return ResponseEntity.ok().body(plan);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.deletePlan(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
