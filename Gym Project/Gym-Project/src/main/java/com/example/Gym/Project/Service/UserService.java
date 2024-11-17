176b13f916fca423d18f5bfb19c985250483d57d
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-11-17 16:22:46 -0300

    correção userService

--- Gym Project/Gym-Project/src/main/java/com/example/Gym/Project/Service/UserService.java
+++ Gym Project/Gym-Project/src/main/java/com/example/Gym/Project/Service/UserService.java
@@ -4,7 +4,9 @@ import com.example.Gym.Project.DTO.UserDTO;
 import com.example.Gym.Project.Model.User;
 import com.example.Gym.Project.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -13,10 +15,12 @@ public class UserService {
     private UserRepository userRepository;
 
 
-
+    @Transactional
     public UserDTO insert(UserDTO dto) {
         User user = new User();
         copyDtotoEntity(dto, user);
+        var senhaCriptografada = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(senhaCriptografada);
         userRepository.save(user);
         return new UserDTO(user);
     }
